c16010a6f22842e681ac4e04b9732c513821c6ed
kazufoot21 <kazufoot21@outlook.jp>
2021-12-22 07:16:39 +0900

    create login page

--- src/components/pages/Login.tsx
+++ src/components/pages/Login.tsx
@@ -1,5 +1,29 @@
+import {
+  Box,
+  Button,
+  Divider,
+  Flex,
+  Heading,
+  Input,
+  Stack,
+} from '@chakra-ui/react';
 import { memo, VFC } from 'react';
 
 export const Login: VFC = memo(() => {
-  return <p>Login page</p>;
+  return (
+    <Flex align="center" justify="center" height="100vh">
+      <Box bg="gray.100" w="sm" p={4} borderRadius="md" shadow="md">
+        <Heading as="h1" size="lg" textAlign="center">
+          User APP
+        </Heading>
+        <Divider borderColor="blue.200" my={4} />
+        <Stack spacing={6} py={4} px={10}>
+          <Input placeholder="User Id" />
+          <Button bg="blue.400" color="white" _hover={{ opacity: 0.8 }}>
+            Login
+          </Button>
+        </Stack>
+      </Box>
+    </Flex>
+  );
 });
