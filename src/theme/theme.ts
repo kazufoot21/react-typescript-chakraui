a606880392ea2e2c6238fda0ddab1e3b157c2097
kazufoot21 <kazufoot21@outlook.jp>
2021-12-22 21:47:36 +0900

    create userlist screen

--- src/theme/theme.ts
+++ src/theme/theme.ts
@@ -1,17 +1,13 @@
 import { extendTheme } from '@chakra-ui/react';
 
 const theme = extendTheme({
-  style: {
+  styles: {
     global: {
       body: {
         backgroundColor: 'gray.100',
         color: 'gray.800',
-        // components: {
-        //   Button: { baseStyle: { _focus: { boxShadow: 'none' } } },
-        // },
       },
     },
   },
 });
-
 export default theme;
