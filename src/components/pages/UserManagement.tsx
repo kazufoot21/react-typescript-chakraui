56745b704bddcdb259f3f6b5b11f81cbaaefba52
kazufoot21 <kazufoot21@outlook.jp>
2021-12-22 22:59:25 +0900

    get userlist func

--- src/components/pages/UserManagement.tsx
+++ src/components/pages/UserManagement.tsx
@@ -1,17 +1,30 @@
-import { Wrap, WrapItem } from '@chakra-ui/react';
-import { memo, VFC } from 'react';
+import { Center, Spinner, Wrap, WrapItem } from '@chakra-ui/react';
+import { memo, VFC, useEffect } from 'react';
 import { UserCard } from '../organisms/user/UserCard';
+import { useAllUser } from '../../hooks/userAllUser';
 
 export const UserManagement: VFC = memo(() => {
+  const { getUsers, users, loading } = useAllUser();
+  useEffect(() => getUsers(), []);
   return (
-    <Wrap p={{ base: 4, md: 10 }}>
-      <WrapItem>
-        <UserCard
-          imageUrl="https://source.unsplash.com/random"
-          userName="kazu"
-          fullName="maeda"
-        />
-      </WrapItem>
-    </Wrap>
+    <>
+      {loading ? (
+        <Center h="100vh">
+          <Spinner />
+        </Center>
+      ) : (
+        <Wrap p={{ base: 4, md: 10 }} justify="center">
+          {users.map((user) => (
+            <WrapItem mx="auto" key={user.id}>
+              <UserCard
+                imageUrl="https://source.unsplash.com/random"
+                userName={user.username}
+                fullName={user.name}
+              />
+            </WrapItem>
+          ))}
+        </Wrap>
+      )}
+    </>
   );
 });
